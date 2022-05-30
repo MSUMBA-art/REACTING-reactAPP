08410bd2da87fc16a21fc7a7411693ae1153b679
ALEXANDER <msumba83@gmail.com>
2022-05-30 20:32:19 +0200

    done practicing react

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Heading from "./Heading";
 import List from "./List";
 import HeadingOne from "./HeadingOne";
 import NameCopyright from "./NameCopyright";
+import pi, { doublePI, triplePI } from "./math";
 //import "./styles.css";
 
 function App() {
@@ -11,6 +12,12 @@ function App() {
       <HeadingOne />
       <Heading />
       <List />
+      <ol>
+        <li>{ pi}</li>
+        <li>{doublePI()}</li>
+        <li>{triplePI()}</li>
+      </ol>
+      <math />
       <NameCopyright />
     </div>
   );
